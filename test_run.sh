3f7eb29bf265a7350da131887f02c67c47223fde
hholgersen <henning.holgersen@storebrand.no>
2022-11-15 14:21:35 +0100

    tidy up some testing files

--- test_run.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-echo "------------------ SAMPLE WITH KEY ------------------" 
-cat sample_stream.jsonl | target-mssql --config config.json
-echo "------------------ SAMPLE WITHOUT KEY ------------------"
-cat unkeyed_stream.jsonl | target-mssql --config config.json
